215c5fb68725deefc3a945d1f508bafcff074135
haneenaihsan <ihsanhaneena@gmail.com>
2025-08-10 09:55:40 +0530

    frst commit

---
+++ js/jq-scrip.js
@@ -0,0 +1,12 @@
+// $(document).ready(function(){
+//     $(window).works(function(){
+//         var scrollHeight = $(window).scrollTop();
+
+//         if(scrollHeight >= 50){
+//             $('header').addClass('updated');
+//         } else {
+//             $('header').removeClass('updated');
+//         }
+//     });
+// });
+
